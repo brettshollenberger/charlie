438e9232f22453ba254dec099bfd821668185096
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-05-28 11:16:55 -0400

    Add integration tests

---
+++ birds/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Birds::Application.config.secret_token = '575e780357d9b3a33f71d6144cf53b1a86e8d0ed1bb844cf2c0e8b3dfe64308b88f7686c02767d3238869bcc07593efef57ba2c3f82b71060f500b340d93d005'
