7f38fa770c32eccd79ca2c6e604a53177f6fa794
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-05-28 12:07:13 -0400

    Capybara tests

--- birds/spec/models/event_spec.rb
+++ birds/spec/models/event_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Event do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Page visits", :type => :feature do
+    it "visits the page" do
+      visit "/events"
+      expect(page.status_code).to eql(200)
+    end
+
+    it "displays an individual event" do
+      event = Event.create(location: "Wherever")
+      visit event_path(event.id)
+      expect(page.status_code).to eql(200)
+    end
+
+    it "adds an event with location" do
+      previous_count = Event.count
+      visit "/events/new"
+      fill_in 'event[location]', :with => "My house"
+      click_button 'Create Event'
+      new_count = Event.count
+      expect(new_count - previous_count).to eql(1)
+      expect(Event.last.location).to eql("My house")
+      visit "/events"
+      expect(page).to have_content("My house")
+    end
+  end
 end
