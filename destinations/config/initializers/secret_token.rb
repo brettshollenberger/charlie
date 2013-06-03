0449363fccdae582c7f11cc8db6c6489e9f40f76
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-06-03 13:53:28 -0400

    Added route

---
+++ destinations/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Destinations::Application.config.secret_token = '166be62e0937959500912d9b6b7f9c5b153e2d6e16326da7c056e66ff19c52a1b6b1f8cab48b5ed6972b1d1af4f9316ef0990ceab349e812c03cb7a578a4d276'
