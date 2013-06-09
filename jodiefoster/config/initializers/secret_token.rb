752ecbb784e4cf9e307a36e681f66ea4a7f3f3b3
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-06-09 19:04:03 -0400

    Add gems

---
+++ jodiefoster/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jodiefoster::Application.config.secret_token = '49a5394a0c40dcb8b3e339c0b6bfc310cfee4ebb2f291e987e1ded4b76bfd9f2a60c59b5b2a9a275c73430339acdade51f9477101246e9f07e70c96fe812656d'
